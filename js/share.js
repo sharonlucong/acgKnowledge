0b2f4688fb1e2c5b10cdcd771434db375bac535a
Cong Lu <CLu@esri.com>
2017-09-06 20:06:25 -0700

    Site updated: 2017-09-06 20:06:25

---
+++ js/share.js
@@ -0,0 +1,25 @@
+function sharePost(type) {
+  var link = window.location.href;
+  var title = document.getElementsByClassName("title") && document.getElementsByClassName("title")[0].textContent.trim();
+
+  if (type == "sina") {
+    window.open(
+      "http://v.t.sina.com.cn/share/share.php?url=" + link  + "&title=" + title
+    );
+  }
+  if (type == "qzone") {
+    window.open(
+      "http://sns.qzone.qq.com/cgi-bin/qzshare/cgi_qzshare_onekey?url=" +
+        link +
+        "&title=" +
+        title
+    );
+  }
+  if (type == "douban") {
+    window.open(
+      "http://www.douban.com/recommend/?url=" + link + "&title=" + title
+    );
+  }
+
+  return false;
+}
